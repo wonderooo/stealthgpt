869f82332787cfe614194487ea80afc3c929e148
kacper szczesny <kacperek.szczesny@gmail.com>
2024-07-01 18:11:36 +0200

    chat ws

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module wonderooo/stealthgpt
 
 go 1.21.6
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,210 @@
 package main
 
 import (
-	"io"
+	"bytes"
+	"encoding/base64"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
+	maxMessageSize = 512
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var conns = make([]*websocket.Conn, 1)
+
 func main() {
-	http.HandleFunc("/hello", getHello)
+	hub := newHub()
+	go hub.run()
+
+	http.HandleFunc("/save-ss", saveSs)
+	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
+		chat(hub, w, r)
+	})
+	http.HandleFunc("/chatui", chatui)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 
 	http.ListenAndServe(":3333", nil)
 }
 
-func getHello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, HTTP!\n")
+func saveSs(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body", http.StatusBadRequest)
+			return
+		}
+
+		base64String := string(body)
+
+		if strings.HasPrefix(base64String, "data:image/jpeg;base64,") {
+			base64String = strings.TrimPrefix(base64String, "data:image/jpeg;base64,")
+		}
+
+		imageBytes, err := base64.StdEncoding.DecodeString(base64String)
+		if err != nil {
+			http.Error(w, "Unable to decode base64 string", http.StatusBadRequest)
+			return
+		}
+
+		fileName := time.Now().String() + ".jpeg"
+		err = ioutil.WriteFile("static/"+fileName, imageBytes, 0644)
+		if err != nil {
+			http.Error(w, "Unable to save image", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Image saved successfully"))
+	}
+}
+
+func chatui(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/chatui" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "chatui.html")
+}
+
+// WS
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+type Client struct {
+	hub  *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.hub.broadcast <- message
+	}
+}
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func chat(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	go client.writePump()
+	go client.readPump()
 }
